31d75abd82cf1c084673f13a180fe101742c125f
Irusha Malalgoda <irusha.works@gmail.com>
2025-02-04 07:37:23 +0000

    Modified UI & Backend in index

--- app/app.py
+++ app/app.py
@@ -6,9 +6,20 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def home():
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Get current time
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # Get hostname
     hostname = socket.gethostname()
-    return render_template("index.html", time=current_time, hostname=hostname)
+
+    # Get IP address (for localhost)
+    ip_address = socket.gethostbyname(hostname)
+
+    # Get OS version
+    os_version = platform.system() + " " + platform.release()
+
+    #Return to index html
+    return render_template("index.html", time=current_time, hostname=hostname, ip_address=ip_address,os_version=os_version)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
